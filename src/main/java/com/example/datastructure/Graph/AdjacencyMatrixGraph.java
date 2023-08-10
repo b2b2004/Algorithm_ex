347368673b61dd1220a8e3e533bc544a3a81a80b
Kwon yongho <b2b2004@nate.com>
2023-08-10 15:51:20 +0900

    10. Graph

---
+++ src/main/java/com/example/datastructure/Graph/AdjacencyMatrixGraph.java
@@ -0,0 +1,62 @@
+package com.example.datastructure.Graph;
+
+import java.util.*;
+
+public class AdjacencyMatrixGraph implements IGraph {
+    private Integer[][] matrix;
+    private Set<Integer> vertexes;
+    private Map<Integer, Integer> indegrees;
+
+    public AdjacencyMatrixGraph(int numOfVertex) {
+        this.vertexes = new HashSet<>();
+        this.indegrees = new HashMap<>();
+        this.matrix = new Integer[numOfVertex][];
+        for (int i = 0; i < numOfVertex; i++) {
+            this.matrix[i] = new Integer[numOfVertex];
+        }
+    }
+
+    @Override
+    public void add(int from, int to, Integer distance) {
+        vertexes.add(from);
+        vertexes.add(to);
+        int count = indegrees.getOrDefault(to, 0);
+        indegrees.put(to, count + 1);
+        matrix[from][to] = distance;
+    }
+
+    @Override
+    public void add(int from, int to) {
+        vertexes.add(from);
+        vertexes.add(to);
+        int count = indegrees.getOrDefault(to, 0);
+        indegrees.put(to, count + 1);
+        matrix[from][to] = 1;
+    }
+
+    @Override
+    public Integer getDistance(int from, int to) {
+        return this.matrix[from][to];
+    }
+
+    @Override
+    public Map<Integer, Integer> getIndegrees() {
+        return this.indegrees;
+    }
+
+    @Override
+    public Set<Integer> getVertexes() {
+        return this.vertexes;
+    }
+
+    @Override
+    public List<Integer> getNodes(int vertex) {
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < matrix[vertex].length; i++) {
+            if (matrix[vertex][i] != null) {
+                ret.add(i);
+            }
+        }
+        return ret;
+    }
+}
