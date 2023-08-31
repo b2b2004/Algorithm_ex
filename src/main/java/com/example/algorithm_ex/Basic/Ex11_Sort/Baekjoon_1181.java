13922e1ec3486bb1330706ce08d8d869970c8e44
Kwon yongho <b2b2004@nate.com>
2023-08-31 12:19:16 +0900

    18. Sort

---
+++ src/main/java/com/example/algorithm_ex/Basic/Ex11_Sort/Baekjoon_1181.java
@@ -0,0 +1,36 @@
+package com.example.algorithm_ex.Basic.Ex11_Sort;
+
+import java.util.*;
+
+public class Baekjoon_1181 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        String s[] = new String[N];
+
+        for(int i=0; i<N; i++){
+            s[i] = sc.next();
+        }
+
+        Arrays.sort(s, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if(s1.length() == s2.length()){
+                    return s1.compareTo(s2);
+                }else{
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+
+        Object result[];
+
+        result = Arrays.stream(s).distinct().toArray();
+
+        for(int i=0; i<result.length; i++){
+            System.out.println(result[i]);
+        }
+
+    }
+}
